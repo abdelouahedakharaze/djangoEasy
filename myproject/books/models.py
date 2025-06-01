1139d404550883976e0bed6ce1f1d658818f0fa2
abdelouahed <112736714+abdelouahedakharaze@users.noreply.github.com>
2025-06-01 14:06:20 +0100

    new again

---
+++ myproject/books/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+# from django.core.validators import MaxValueValidator,MinValueValidator
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    year = models.SmallIntegerField()
+    
+    def __str__(self):
+        return self.title
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# year = models.SmallIntegerField(validators=[MinValueValidator(1000), MaxValueValidator(2030)])
