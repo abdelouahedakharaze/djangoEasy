c00e067d2a220a8b99e0e774a0e790bb66ca9c81
abdelouahed <112736714+abdelouahedakharaze@users.noreply.github.com>
2025-06-02 17:23:41 +0100

    g

--- myproject/books/models.py
+++ myproject/books/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-# from django.core.validators import MaxValueValidator,MinValueValidator
+from django.core.validators import MaxValueValidator,MinValueValidator
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    year = models.SmallIntegerField()
+    year = models.SmallIntegerField(validators=[MinValueValidator(1000), MaxValueValidator(2030)])    
     
     def __str__(self):
         return self.title
@@ -33,4 +34,3 @@ class Book(models.Model):
 
 
 
-# year = models.SmallIntegerField(validators=[MinValueValidator(1000), MaxValueValidator(2030)])
--- myproject/books/views.py
+++ myproject/books/views.py
@@ -3,5 +3,4 @@ from .models import Book
 
 def book_list(request):
     books = Book.objects.all()
-    
     return render(request, 'books/book_list.html', {'books': books})
